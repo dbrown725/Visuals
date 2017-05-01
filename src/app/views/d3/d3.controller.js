bbe9911d25f1b524c6763d0e6ec97dfee42d49b8
Brown, David (US - Baltimore) <davidbrown3@deloitte.com>
2017-05-01 09:01:47 -0400

    added new line-chart and download pdf button

--- src/app/views/d3/d3.controller.js
+++ src/app/views/d3/d3.controller.js
@@ -14,9 +14,30 @@
     .module('Visuals')
     .controller('D3', D3);
 
-  function D3($scope, $stateParams) {
+  function D3($scope, $stateParams, $http, $timeout) {
     var vm = this;
     vm.type = $stateParams.type;
+    vm.downloadLink = '';
+    vm.downloadPDF = function() {
+      var e1 = angular.element(document.querySelectorAll('svg'));
+      var configPost = {
+                headers : {
+                    'Content-Type': 'application/xml'
+                }
+            };
+
+      $http.post('http://localhost:8080/pdf-generator/pdf/', e1[0].outerHTML, configPost).then(function(response) {
+        vm.downloadLink = response.data.pdf;
+        $timeout(clickHiddenElement, 500);
+      }, function errorCallback(response) {
+        console.log('POST error response', response);
+      });
+    };
+  }
+
+  function clickHiddenElement() {
+    var elem = document.getElementById('hiddenLink');
+    elem.click();
   }
 
 })();
