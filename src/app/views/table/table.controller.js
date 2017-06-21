da6aa52756be5301891c6f0981c071be1ab5d647
Brown, David (US - Baltimore) <davidbrown3@deloitte.com>
2017-06-21 16:45:08 -0400

    cleanup dead code

--- src/app/views/table/table.controller.js
+++ src/app/views/table/table.controller.js
@@ -27,41 +27,6 @@
       alert('From parent controller\n\nDrillDowned on: ' + column + '\n\nRequest object to send:\n' + JSON.stringify(object));
     };
 
-    function dumpObjectIndented(obj, indent)
-    {
-      var result = '';
-      if (indent === null) {
-        indent = '';
-      }
-
-      for (var property in obj)
-      {
-        var value = obj[property];
-        if (typeof value === 'string') {
-          value = '"' + value + '"';
-        } else if (typeof value === 'object')
-        {
-          if (value instanceof Array)
-          {
-            // Just let JS convert the Array to a string!
-            value = '[ ' + value + ' ]';
-          }
-          else
-          {
-            // Recursive dump
-            // (replace '  ' by '\t' or something else if you prefer)
-            var od = dumpObjectIndented(value, indent + '  ');
-            // If you like { on the same line as the key
-            //value = '{\n' + od + '\n' + indent + '}';
-            // If you prefer { and } to be aligned
-            value = '\n' + indent + '{\n' + od + '\n' + indent + '}';
-          }
-        }
-        result += indent + '"' + property + '" : ' + value + ',\n';
-      }
-      return result.replace(/,\n$/, '');
-    }
-
     vm.retrieveData = function(transaction) {
       console.log('transaction', transaction);
       if(transaction === 'threeDeep') {
