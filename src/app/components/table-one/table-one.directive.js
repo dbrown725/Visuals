08ff19d79d8a33867ce12cdf87794ea4b377b85a
Brown, David (US - Baltimore) <davidbrown3@deloitte.com>
2017-06-25 17:41:03 -0400

    Added drillDown functionality and example.

--- src/app/components/table-one/table-one.directive.js
+++ src/app/components/table-one/table-one.directive.js
@@ -19,6 +19,7 @@
     var controller = ['$scope', function ($scope) {
       $scope.$watch('responsedata', function(newValue, oldValue) {
         if(Object.keys(newValue).length) {
+            console.log('in $scope.$watch');
             clearTable();
             $scope.response = newValue;
             setTableHeaders();
@@ -42,106 +43,132 @@
       $scope.currentSortColumn = '';
       $scope.sortAscending = true;
 
-      $scope.sortColumn = function(columnNumber) {
-        console.log('columnNumber', columnNumber);
+      $scope.sortColumn = function(columnNumber, sortAscKey, sortDescKey) {
+
+
+        console.log('sortAscKey', sortAscKey);
+        console.log('sortDescKey', sortDescKey);
         if(columnNumber === $scope.currentSortColumn) {
           $scope.sortAscending = !$scope.sortAscending;
         } else {
           $scope.currentSortColumn = columnNumber;
           $scope.sortAscending = true;
         }
-
-        function determineSortLevel() {
-          for (var k = 0; k < $scope.response.rows.length; k++) {
-            if($scope.response.rows[k][columnNumber].value) {
-              return 'level1';
-            }
+        // if keys exist then is drillDown and sorts are done
+        // on the backend by the SQL
+        if(sortAscKey && sortDescKey) {
+          if($scope.sortAscending) {
+            $scope.drilldownsort({sortKey:sortAscKey});
+          } else {
+            $scope.drilldownsort({sortKey:sortDescKey});
           }
-          for (var m = 0; m < $scope.response.rows.length; m++) {
-            for (var n = 0; n < $scope.response.rows[m].rows.length; n++) {
-              if($scope.response.rows[m].rows[n][columnNumber].value) {
-                return 'level2';
+        } else {
+          function determineSortLevel() {
+            for (var k = 0; k < $scope.response.rows.length; k++) {
+              if($scope.response.rows[k][columnNumber].value) {
+                return 'level1';
               }
             }
-            return 'level3';
+            for (var m = 0; m < $scope.response.rows.length; m++) {
+              for (var n = 0; n < $scope.response.rows[m].rows.length; n++) {
+                if($scope.response.rows[m].rows[n][columnNumber].value) {
+                  return 'level2';
+                }
+              }
+              return 'level3';
+            }
           }
-        }
 
-        var sortLevel = determineSortLevel();
-        console.log('sortLevel',sortLevel);
+          var sortLevel = determineSortLevel();
+          function compare(a,b) {
+            var val1 = a[columnNumber].value;
+            var val2 = b[columnNumber].value;
+            if(a[columnNumber].number) {
+              val1 = Number(a[columnNumber].value);
+              val2 = Number(b[columnNumber].value);
+            }
+            if($scope.sortAscending) {
+              if (val1 < val2) {
+                return -1;
+              }
+              if (val1 > val2) {
+                return 1;
+              }
+              return 0;
+            } else {
+              if (val1 < val2) {
+                return 1;
+              }
+              if (val1 > val2) {
+                return -1;
+              }
+              return 0;
+            }
+          }
 
-        function compare(a,b) {
-          var val1 = a[columnNumber].value;
-          var val2 = b[columnNumber].value;
-          if(a[columnNumber].number) {
-            val1 = Number(a[columnNumber].value);
-            val2 = Number(b[columnNumber].value);
+          if(sortLevel === 'level1') {
+            $scope.response.rows.sort(compare);
+          } else if(sortLevel === 'level2') {
+            for (var j = 0; j < $scope.response.rows.length; j++) {
+              $scope.response.rows[j].rows.sort(compare);
+            }
+          } else if(sortLevel === 'level3') {
+            for (var m = 0; m < $scope.response.rows.length; m++) {
+              for (var n = 0; n < $scope.response.rows[m].rows.length; n++) {
+                $scope.response.rows[m].rows[n].rows.sort(compare);
+              }
+            }
           }
-          if($scope.sortAscending) {
-            //console.log('ascending');
-            if (val1 < val2) {
-              return -1;
-            }
-            if (val1 > val2) {
-              return 1;
-            }
-            return 0;
-          } else {
-            //console.log('descending');
-            if (val1 < val2) {
-              return 1;
-            }
-            if (val1 > val2) {
-              return -1;
-            }
-            return 0;
-          }
+          setTableBody($scope.response, false);
         }
-
-        if(sortLevel === 'level1') {
-          $scope.response.rows.sort(compare);
-        } else if(sortLevel === 'level2') {
-          for (var j = 0; j < $scope.response.rows.length; j++) {
-            $scope.response.rows[j].rows.sort(compare);
-          }
-        } else if(sortLevel === 'level3') {
-          for (var m = 0; m < $scope.response.rows.length; m++) {
-            for (var n = 0; n < $scope.response.rows[m].rows.length; n++) {
-              $scope.response.rows[m].rows[n].rows.sort(compare);
-            }
-          }
-        }
-        setTableBody($scope.response, false);
       };
 
       function clearTable() {
           $scope.columnOneName = 'Header One';
           $scope.columnOneShow = false;
           $scope.columnOneSortable = false;
+          $scope.columnOneSortAsc = '';
+          $scope.columnOneSortDesc = '';
           $scope.columnTwoName = 'Header Two';
           $scope.columnTwoShow = false;
           $scope.columnTwoSortable = false;
+          $scope.columnTwoSortAsc = '';
+          $scope.columnTwoSortDesc = '';
           $scope.columnThreeName = 'Header Three';
           $scope.columnThreeShow = false;
           $scope.columnThreeSortable = false;
+          $scope.columnThreeSortAsc = '';
+          $scope.columnThreeSortDesc = '';
           $scope.columnFourName = 'Header Four';
           $scope.columnFourShow = false;
           $scope.columnFourSortable = false;
+          $scope.columnFourSortAsc = '';
+          $scope.columnFourSortDesc = '';
           $scope.columnFiveName = 'Header Five';
           $scope.columnFiveShow = false;
           $scope.columnFiveSortable = false;
+          $scope.columnFiveSortAsc = '';
+          $scope.columnFiveSortDesc = '';
           $scope.columnSixName = 'Header Six';
           $scope.columnSixShow = false;
           $scope.columnSixSortable = false;
+          $scope.columnSixSortAsc = '';
+          $scope.columnSixSortDesc = '';
           $scope.columnSevenName = 'Header Seven';
           $scope.columnSevenShow = false;
           $scope.columnSevenSortable = false;
+          $scope.columnSevenSortAsc = '';
+          $scope.columnSevenSortDesc = '';
           $scope.columnEightName = 'Header Eight';
           $scope.columnEightShow = false;
           $scope.columnEightSortable = false;
+          $scope.columnEightSortAsc = '';
+          $scope.columnEightSortDesc = '';
           $scope.columnNineName = 'Header Nine';
           $scope.columnNineShow = false;
           $scope.columnNineSortable = false;
+          $scope.columnNineSortAsc = '';
+          $scope.columnNineSortDesc = '';
       }
 
       function setTableHeaders() {
@@ -149,46 +176,64 @@
           $scope.columnOneName = $scope.response.columnHeaders.header1.value;
           $scope.columnOneShow = true;
           $scope.columnOneSortable = $scope.response.columnHeaders.header1.clickable;
+          $scope.columnOneSortAsc = $scope.response.columnHeaders.header1.sortAsc;
+          $scope.columnOneSortDesc = $scope.response.columnHeaders.header1.sortDesc;
         }
         if($scope.response.columnHeaders.header2) {
           $scope.columnTwoName = $scope.response.columnHeaders.header2.value;
           $scope.columnTwoShow = true;
           $scope.columnTwoSortable = $scope.response.columnHeaders.header2.clickable;
+          $scope.columnTwoSortAsc = $scope.response.columnHeaders.header2.sortAsc;
+          $scope.columnTwoSortDesc = $scope.response.columnHeaders.header2.sortDesc;
         }
         if($scope.response.columnHeaders.header3) {
           $scope.columnThreeName = $scope.response.columnHeaders.header3.value;
           $scope.columnThreeShow = true;
           $scope.columnThreeSortable = $scope.response.columnHeaders.header3.clickable;
+          $scope.columnThreeSortAsc = $scope.response.columnHeaders.header3.sortAsc;
+          $scope.columnThreeSortDesc = $scope.response.columnHeaders.header3.sortDesc;
         }
         if($scope.response.columnHeaders.header4) {
           $scope.columnFourName = $scope.response.columnHeaders.header4.value;
           $scope.columnFourShow = true;
           $scope.columnFourSortable = $scope.response.columnHeaders.header4.clickable;
+          $scope.columnFourSortAsc = $scope.response.columnHeaders.header4.sortAsc;
+          $scope.columnFourSortDesc = $scope.response.columnHeaders.header4.sortDesc;
         }
         if($scope.response.columnHeaders.header5) {
           $scope.columnFiveName = $scope.response.columnHeaders.header5.value;
           $scope.columnFiveShow = true;
           $scope.columnFiveSortable = $scope.response.columnHeaders.header5.clickable;
+          $scope.columnFiveSortAsc = $scope.response.columnHeaders.header5.sortAsc;
+          $scope.columnFiveSortDesc = $scope.response.columnHeaders.header5.sortDesc;
         }
         if($scope.response.columnHeaders.header6) {
           $scope.columnSixName = $scope.response.columnHeaders.header6.value;
           $scope.columnSixShow = true;
           $scope.columnSixSortable = $scope.response.columnHeaders.header6.clickable;
+          $scope.columnSixSortAsc = $scope.response.columnHeaders.header6.sortAsc;
+          $scope.columnSixSortDesc = $scope.response.columnHeaders.header6.sortDesc;
         }
         if($scope.response.columnHeaders.header7) {
           $scope.columnSevenName = $scope.response.columnHeaders.header7.value;
           $scope.columnSevenShow = true;
           $scope.columnSevenSortable = $scope.response.columnHeaders.header7.clickable;
+          $scope.columnSevenSortAsc = $scope.response.columnHeaders.header7.sortAsc;
+          $scope.columnSevenSortDesc = $scope.response.columnHeaders.header7.sortDesc;
         }
         if($scope.response.columnHeaders.header8) {
           $scope.columnEightName = $scope.response.columnHeaders.header8.value;
           $scope.columnEightShow = true;
           $scope.columnEightSortable = $scope.response.columnHeaders.header8.clickable;
+          $scope.columnEightSortAsc = $scope.response.columnHeaders.header8.sortAsc;
+          $scope.columnEightSortDesc = $scope.response.columnHeaders.header8.sortDesc;
         }
         if($scope.response.columnHeaders.header9) {
           $scope.columnNineName = $scope.response.columnHeaders.header9.value;
           $scope.columnNineShow = true;
           $scope.columnNineSortable = $scope.response.columnHeaders.header9.clickable;
+          $scope.columnNineSortAsc = $scope.response.columnHeaders.header9.sortAsc;
+          $scope.columnNineSortDesc = $scope.response.columnHeaders.header9.sortDesc;
         }
       }
 
@@ -206,9 +251,7 @@
               }
               $scope.rows.push(data.rows[i].rows[j]);
               if(data.rows[i].rows[j].groupingRow) {
-                console.log('has second grouping');
                 for (var k = 0; k < data.rows[i].rows[j].rows.length; k++) {
-                  console.log('data.rows[i].rows[j].rows', data.rows[i].rows[j].rows);
                   $scope.rows.push(data.rows[i].rows[j].rows[k]);
                 }
               }
@@ -220,9 +263,7 @@
       $scope.rowItemClicked = function(row, column) {
 
         if(row.groupingRow) {
-          console.log('Is a grouping row item');
           for (var i = 0; i < row.rows.length; i++) {
-            console.log('row.rows[i]', row.rows[i]);
             row.rows[i].show = !row.rows[i].show;
             if(row.rows[i].groupingRow) {
               for (var j = 0; j < row.rows[i].rows.length; j++) {
@@ -231,10 +272,6 @@
             }
           }
         } else {
-          console.log('Is not a grouping row item');
-          console.log('column', column);
-          console.log('row.originalResult', row.originalResult);
-          //$scope.mycallback(row.originalResult, column);
           $scope.mycallback({obj:row.originalResult,column:column});
         }
       };
@@ -247,7 +284,8 @@
         templateUrl: 'app/components/table-one/table-one.directive.html',
         scope: {
           responsedata: '=',
-          mycallback: '&'
+          mycallback: '&',
+          drilldownsort: '&'
         },
         controller: controller,
         link: link
--- src/app/views/table/table.controller.js
+++ src/app/views/table/table.controller.js
@@ -21,10 +21,53 @@
 
     vm.response = {};
 
+    var drillDownRequest = {
+      requestType: 'STUDENT_DISTINCT_STUDENT_COUNT',
+      studentDemoFilter: {
+        enrlmntDateFrom: 1325394000000,
+        enrlmntDateTo: 1491105600000,
+        wthdrwlDateFrom: null,
+        wthdrwlDateTo: null,
+        eligExpDateFrom: null,
+        eligExpDateTo: null,
+        boolOpenEnrlmnt: false,
+        rptGrping: 'State',
+        selectedStates: '5,10',
+        selectedEnrlTypes: null,
+        selectedCourseTypes: '1,2,3,4,5,6,7,8,9',
+        selectedAssessmentTypes: '1,2,3,4,5,6,7,8',
+        asmntAdminDateFrom: null,
+        asmntAdminDateTo: null,
+        beginAcademicYear: 0,
+        endAcademicYear: 0,
+        periodType: 'No Period',
+        categoryType: 'Category 1',
+        countType: null,
+        requestType: null
+      },
+      studentIDFilter: {
+        stateKey: '3',
+        districtId: null,
+        category: 'No Data Submitted',
+        category2: 'No Data Submitted',
+        beginRow: 1,
+        endRow: 20,
+        sortFields: 'MSIX_ID_ASC'
+      }
+    };
+
     vm.handleDrillDownClick = function(object, column) {
       console.log('object', object);
       console.log('column', column);
       alert('From parent controller\n\nDrillDowned on: ' + column + '\n\nRequest object to send:\n' + JSON.stringify(object));
+    };
+
+    vm.handleDrillDownSortClick = function(newSortKey) {
+      console.log('sortKey', newSortKey);
+      drillDownRequest.studentIDFilter.sortFields = newSortKey;
+      drillDown();
+      //console.log('column', column);
+      //alert('From parent controller\n\nDrillDowned on: ' + column + '\n\nRequest object to send:\n' + JSON.stringify(object));
     };
 
     vm.retrieveData = function(transaction) {
@@ -35,12 +78,16 @@
         twoDeep();
       } else if (transaction === 'oneDeep') {
         oneDeep();
+      } else if (transaction === 'drillDown') {
+        drillDown();
       }
     };
 
     //********* REPLACE START **************************************
+
+
     function threeDeep() {
-        alert('see Dave for threeDeep specific sample code/data');
+      alert('see Dave for threeDeep specific sample code/data');
     }
 
     function twoDeep() {
@@ -50,6 +97,12 @@
     function oneDeep() {
       alert('see Dave for oneDeep specific sample code/data');
     }
+
+    function drillDown() {
+      alert('see Dave for drillDown specific sample code/data');
+    }
+
+
     //********* REPLACE END **************************************
   }
 
